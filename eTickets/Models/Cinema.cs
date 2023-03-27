ac40df0115cb35e144c5b8009d3b523ffc1bbb01
allMazZzey <allmazzzey@gmail.com>
2023-03-27 14:16:08 +0300

    Added movie model and movie category enum

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -1,6 +1,13 @@
-﻿namespace eTickets.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
 {
     public class Cinema
     {
+        [Key]
+        public int Id { get; set; }
+        public string LogoUrl { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
