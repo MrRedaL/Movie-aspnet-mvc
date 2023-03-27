e16ca730a41bab1f62b060a76847694023c5dca3
allMazZzey <allmazzzey@gmail.com>
2023-03-27 15:51:08 +0300

    Added foreign keys to actor_movie model

--- eTickets/Models/Actor_Movie.cs
+++ eTickets/Models/Actor_Movie.cs
@@ -1,11 +1,15 @@
-﻿namespace eTickets.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
 {
     public class Actor_Movie
     {
         public int ActorId { get; set; }
+        [ForeignKey("ActorId")]
         public Actor Actor { get; set; }
 
         public int MovieId { get; set; }
+        [ForeignKey("MovieId")]
         public Movie Movie { get; set; }
     }
 }
