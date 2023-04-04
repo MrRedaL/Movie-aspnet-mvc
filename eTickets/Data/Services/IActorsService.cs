4c480e029b6735aaf473fa9774821e2a24aa0d75
allMazZzey <allmazzzey@gmail.com>
2023-04-04 19:12:19 +0300

    Added IActorsService

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,13 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAllActors();
+        Actor GetActorById(int id);
+        void AddActor(Actor actor);
+        Actor UpdateActorById(int id, Actor newActor);
+        void DeleteActorById(int id);
+    }
+}
